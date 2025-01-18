b1f3687863aeda49bac4c40f8e828d6a518d6b26
eukadish <eugene.kadish@gmail.com>
2025-01-18 07:32:00 -0500

    all balance tests for the migration

--- test/Migration.t.sol
+++ test/Migration.t.sol
@@ -62,8 +62,8 @@ contract MigrationTest is Test {
             address(savingModule)
         );
 
-        srusd.mint(eoa1, 1_000_000e18);
-        srusd.mint(eoa2, 1_000_000e18);
+        srusd.mint(eoa1, 2_000_000e18);
+        srusd.mint(eoa2, 2_000_000e18);
 
         vm.prank(eoa1);
         srusd.approve(address(migration), type(uint256).max);
@@ -73,31 +73,76 @@ contract MigrationTest is Test {
     }
 
     function testInitialState() external view {
-        // assertEq(address(migration.rusd()), address(rusd));
-        // assertEq(address(migration.srusd()), address(rusd));
+        assertEq(address(migration.rusd()), address(rusd));
+        assertEq(address(migration.srusd()), address(srusd));
 
-        // assertEq(address(migration.vault()), address(rusd));
-        // assertEq(address(migration.savingModule()), address(rusd));
-
-        assertTrue(true);
+        assertEq(address(migration.vault()), address(vault));
+        assertEq(address(migration.savingModule()), address(savingModule));
     }
 
     function testMigrate() external {
-        // check price
-        console.log(vault.previewDeposit(1_000_000e18));
-
-        uint256 balance1 = srusd.balanceOf(eoa1);
-
-        // console.log(balance1);
-        // console.log(savingModule.currentPrice());
+        assertEq(vault.previewDeposit(1_000_000e18), 1_000_000e18);
 
         vm.prank(eoa1);
-        uint256 shares = migration.migrate(balance1);
+        uint256 shares = migration.migrate(1_000_000e18);
 
-        console.log(shares);
+        assertEq(shares, 1_000_000e18);
+        assertEq(vault.balanceOf(eoa1), 1_000_000e18);
 
-        // assert shares received by the caller
+        savingModule.setCurrentPrice(1.50000000e8);
 
-        assertTrue(true);
+        vm.prank(eoa2);
+        shares = migration.migrate(1_000_000e18);
+
+        assertEq(shares, 1_500_000e18);
+        assertEq(vault.balanceOf(eoa2), 1_500_000e18);
+
+        vault.update(0.000000003022265993024580000e27);
+
+        skip(365 days);
+
+        assertEq(vault.compoundFactor(), 1.099852195451745627479632852e27);
+        assertEq(vault.compoundFactorAccum(), 1.000000000000000000000000000e27);
+
+        assertEq(
+            vault.previewDeposit(1_000_000e18),
+            909_213.078025695050374104e18
+        );
+        assertEq(
+            vault.previewDeposit(1_500_000e18),
+            1_363_819.617038542575561156e18
+        );
+
+        uint256 balance = srusd.balanceOf(eoa1);
+
+        vm.prank(eoa1);
+        shares = migration.migrate(balance);
+
+        assertEq(shares, 1_363_819.617038542575561156e18);
+        assertEq(vault.balanceOf(eoa1), 2_363_819.617038542575561156e18);
+
+        vault.update(0.000000012857214404249400000e27);
+
+        skip(365 days);
+
+        assertEq(vault.compoundFactor(), 1.636212815140872887708016220e27);
+        assertEq(vault.compoundFactorAccum(), 1.099852195451745627479632852e27);
+
+        assertEq(vault.previewDeposit(1_000_000e18), 611_167.441512736885416082e18);
+        assertEq(vault.previewDeposit(2_000_000e18), 1_222_334.883025473770832164e18);
+
+        balance = srusd.balanceOf(eoa2);
+        savingModule.setCurrentPrice(2.00000000e8);
+
+        vm.prank(eoa2);
+        shares = migration.migrate(balance);
+
+        assertEq(shares, 1_222_334.883025473770832164e18);
+        assertEq(vault.balanceOf(eoa2), 2_722_334.883025473770832164e18);
+
+        assertEq(srusd.balanceOf(eoa1), 0);
+        assertEq(srusd.balanceOf(eoa2), 0);
+
+        // TODO: Check the balance in the migration contract
     }
 }
